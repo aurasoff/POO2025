7f9d121836d9f9f3fa3f97336c4f56066cb2f415
aurasoff <aurasofiamontoyamesa@gmail.com>
2025-04-08 16:10:11 -0500

    Add files via upload

---
+++ Especialidad.java
@@ -0,0 +1,11 @@
+package org.uniquindio.edu.co.poo.model;
+
+public enum Especialidad {
+    ODONTOLIGIA_GENERAL(0),ORTODONCIA(1),ENDODONCIA(2),CIRUGIA_ORAL(3);
+    private final int valor;
+
+    Especialidad(int valor) {
+        this.valor = valor;
+    }
+
+}
